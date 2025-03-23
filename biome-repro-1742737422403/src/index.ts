4fa9c6d4c4a5eaadcb861915cededb7c387c626a
tinnyterr <168141209+TinnyTerr@users.noreply.github.com>
2025-03-23 13:46:56 +0000

    first commit

---
+++ biome-repro-1742737422403/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * If possible, put your minimal reproduction in this file, but feel free to
+ * create other files elsewhere in the project if necessary.
+ *
+ * Once you have a minimal reproduction, commit it and push it to a public
+ * GitHub repo.
+ */
+let test = "this is a filler line";
+
+test = "this is also a  filler line";
